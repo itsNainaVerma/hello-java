9e77b98d9fbea86bc76558bf51d7f8e8ed4840c3
Naina Verma <itsnainav008@gmail.com>
2025-06-03 23:30:01 +0530

    String program

---
+++ src/main/java/AddingStringAndNumbers.java
@@ -0,0 +1,6 @@
+public class AddingStringAndNumbers {
+        public static void main(String[] args) {
+            String s="JAVA"; String s1=new String("JAVA");
+            System.out.println(4+4+""+8+4);  // o/p- 884
+        }
+}
