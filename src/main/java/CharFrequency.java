39f9aebe6d2267ffe5fad19653a2b0f6536c9296
Naina Verma <itsnainav008@gmail.com>
2025-06-03 23:21:02 +0530

    String program

---
+++ src/main/java/CharFrequency.java
@@ -0,0 +1,24 @@
+/*
+I/P- AABBBCCCCaaaaa
+O/P- A2B3C4a5
+*/
+
+import java.util.*;
+
+public class CharFrequency {
+    public static void main(String[] args) {
+        String input = "AABBBCCCCaaaaa";
+        Map<Character, Integer> freqMap = new LinkedHashMap<>();
+
+        for (char c : input.toCharArray()) {
+            if (c != ' ')
+                freqMap.put(c, freqMap.getOrDefault(c, 0) + 1);
+        }
+
+        for (char c : freqMap.keySet()) {
+            System.out.print(c+""+ freqMap.get(c));
+        }
+
+        //System.out.print(freqMap);
+    }
+}
