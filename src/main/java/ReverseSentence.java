5a10ff4e31c7b57a3bb34ed21c7dbe9b235949d5
Naina Verma <itsnainav008@gmail.com>
2025-06-04 16:25:40 +0530

    String program

--- src/main/java/ReverseSentence.java
+++ src/main/java/ReverseSentence.java
@@ -2,7 +2,7 @@
 
 public class ReverseSentence {
     public static void main(String[] args) {
-        String s = "Hello. My name is Pie";
+        String s = "Hello. My name is Naina";
 
         String[] w = s.split("\\.");
 
