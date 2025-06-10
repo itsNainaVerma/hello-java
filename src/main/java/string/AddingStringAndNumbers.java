6403c862583f1d775ba1f7d1fd196f42671811b9
Naina Verma <itsnainav008@gmail.com>
2025-06-10 18:19:14 +0530

    updated string class with more operations

--- src/main/java/string/AddingStringAndNumbers.java
+++ src/main/java/string/AddingStringAndNumbers.java
@@ -2,7 +2,14 @@ package string;
 
 public class AddingStringAndNumbers {
         public static void main(String[] args) {
-            String s="JAVA"; String s1=new String("JAVA");
+            String s="JAVA";
+            String s1=new String("JAVA");
             System.out.println(4+4+""+8+4);  // o/p- 884
+            System.out.println(80+80+"Selenium"+40+40);  // 160Selenium4040
+            System.out.println(40+40); //80
+            System.out.println(80+80+1+s+40+40); //161JAVA4040
+            System.out.println(80+80+1+s1+40+40); //161JAVA4040
+            System.out.println("hello"+80+80+"Selenium"+40+40); //hello8080Selenium4040
+            System.out.println("hello"+80+80+"Selenium"+(40+40));   //hello8080Selenium80
         }
 }
