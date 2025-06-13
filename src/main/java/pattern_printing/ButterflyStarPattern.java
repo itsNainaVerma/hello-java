c7dfb939e90a5a13ae3508cb6df7dae013be466e
Naina Verma <itsnainav008@gmail.com>
2025-06-13 18:29:53 +0530

    patternprinting

---
+++ src/main/java/pattern_printing/ButterflyStarPattern.java
@@ -0,0 +1,47 @@
+package pattern_printing;
+
+public class ButterflyStarPattern {
+    public static void main(String[] args) {
+
+        int count = 10;
+
+        for(int i = 1; i <= 6; i++) {
+
+            for(int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            for(int k = 1; k <= count; k++) {
+                System.out.print(" ");
+            }
+
+            for(int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+            count = count - 2;
+        }
+
+
+        count = 0;
+
+        for(int i = 6; i >= 1; i--) {
+
+            for(int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            for(int k = 1; k <= count; k++) {
+                System.out.print(" ");
+            }
+
+            for(int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+            count = count + 2;
+        }
+    }
+}
